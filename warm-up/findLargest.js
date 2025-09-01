9c196eb150a8807f83801c6b48ffe1fe06a51f89
meghraj thakre <meghrajthakre444@gmail.com>
2025-09-01 10:25:45 +0530

    finding largest

--- warm-up/findLargest.js
+++ warm-up/findLargest.js
@@ -1,10 +1,10 @@
 // write a fuction that returns largest number  in an array ;
 let arr = [1,2,3,4,5,6,7,8,9,50]
-function findLargest(arr) {
-    let largest = arr[0];
-    for (let i = 0; i < arr.length; i++){
-        if (largest < arr[i]) {
-            largest = arr[i]
+function findLargest(a) {
+    let largest = a[0];
+    for (let i = 0; i < a.length; i++){
+        if (largest < a[i]) {
+            largest = a[i]
         }
     }
     return largest
