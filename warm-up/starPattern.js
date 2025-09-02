39025c70b319db2f6a7c1921c2b8dbb6ac4adc58
meghraj thakre <meghrajthakre444@gmail.com>
2025-09-02 09:58:34 +0530

    star patterns

--- warm-up/starPattern.js
+++ warm-up/starPattern.js
@@ -3,18 +3,68 @@
 // 1 0 1
 // 1 0 1 0
 
-let n = 5;
-for (let i = 0; i < n; i++){
+for (let i = 0; i < n; i++) {
     let row = '';
     let toggle = 1
-    for (let j = 0; j < (i + 1); j++){
+    for (let j = 0; j < (i + 1); j++) {
         row = row + toggle;
         if (toggle == 1) {
             toggle = 0;
         }
         else {
-            toggle =1
+            toggle = 1
         }
     }
     console.log(row)
+}
+
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * * 
+
+for (let i = 0; i < n; i++) {
+    let row = '';
+    for (let j = 0; j < i+1; j++) {
+        row = row + '* '
+    }
+    console.log(row)
+}
+
+
+
+// * * * * 
+// * * * 
+// * * 
+// * 
+for (let i = 0; i < n; i++) {
+    let row = '';
+    for (let j = 0; j < n - (i + 1); j++) {
+        row = row + '* '
+    }
+    console.log(row)
+}
+
+
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+let n = 5;
+
+for (let i = 0; i < n; i++) {
+    let row = '';
+    for (let j = 0; j < n - (i + 1); j++) {
+        row = row + " "
+
+    }
+    for (let k = 0; k <=i; k++) {
+        row = row + "*"
+    }
+    console.log(row);
+
 }
