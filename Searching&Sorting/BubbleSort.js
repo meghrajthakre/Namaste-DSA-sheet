67b4358c96a2d198844a1471fb385584a595c958
meghraj thakre <meghrajthakre444@gmail.com>
2025-09-18 07:46:24 +0530

    Binary Sort Algo

--- Searching&Sorting/BubbleSort.js
+++ Searching&Sorting/BubbleSort.js
@@ -29,3 +29,40 @@ function bubbleSortOptimized(arr) {
 // Example
 let arr = [1, 2, 3, 4, 5]; // already sorted
 console.log("Sorted Array:", bubbleSortOptimized(arr));
+
+
+// Dry Run Example
+
+// Array = [5, 3, 1, 4, 2]
+
+// Pass 1:
+
+// Compare 5 & 3 → swap → [3, 5, 1, 4, 2]
+
+// Compare 5 & 1 → swap → [3, 1, 5, 4, 2]
+
+// Compare 5 & 4 → swap → [3, 1, 4, 5, 2]
+
+// Compare 5 & 2 → swap → [3, 1, 4, 2, 5]
+// 👉 Largest (5) last me chala gaya
+
+// Pass 2:
+
+// Compare 3 & 1 → swap → [1, 3, 4, 2, 5]
+
+// Compare 3 & 4 → no swap
+
+// Compare 4 & 2 → swap → [1, 3, 2, 4, 5]
+// 👉 2nd largest (4) apni jagah aa gaya
+
+// Pass 3:
+
+// Compare 1 & 3 → no swap
+
+// Compare 3 & 2 → swap → [1, 2, 3, 4, 5]
+
+// Pass 4:
+
+// Compare 1 & 2 → no swap (already sorted)
+
+// ✅ Final Sorted Array = [1, 2, 3, 4, 5]
