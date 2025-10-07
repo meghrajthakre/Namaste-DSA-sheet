d78df145b12e0ea260705939310a6391554006c9
meghraj thakre <meghrajthakre444@gmail.com>
2025-10-07 09:19:13 +0530

    Aproach 2 Last Word Length

--- Strings/LengthOfLastWord.js
+++ Strings/LengthOfLastWord.js
@@ -45,3 +45,21 @@ var lengthOfLastWord = function (s) {
     return count
 };
 
+// solution 3
+
+var lengthOfLastWord = function (s) {
+    let n = s.length - 1;
+    let count = 0
+
+    while (n >= 0) {
+        if (s[n] !== " ") {
+            count++
+        }
+        else if (count > 0) {
+            break
+        }
+        n--
+    }
+    
+    return count
+};
