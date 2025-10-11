f21f77a777a259a04806a9fdeeea7946946f9d4b
meghraj thakre <meghrajthakre444@gmail.com>
2025-10-11 13:41:06 +0530

    Reverse String

---
+++ Strings/ReverseString2.js
@@ -0,0 +1,20 @@
+
+var reverseStr = function (s, k) {
+    s = s.split('')
+
+    for (let x = 0; x < s.length; x = x + (2 * k)) {
+        let mid = Math.floor(k / 2)
+        for (let i = 0; i < mid; i++) {
+            
+            let temp = s[x + i]
+            s[x + i] = s[x + k - 1 - i]
+            s[x + k - 1 - i] = temp
+        
+        }
+    }
+
+
+    return s.join("")
+};
+const res = reverseStr('efghijkl', 2);
+console.log(res)
