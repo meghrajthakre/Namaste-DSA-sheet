f2100c1719cfa3962897f404e6117f7f9f831784
meghraj thakre <meghrajthakre444@gmail.com>
2025-10-16 17:59:15 +0530

    Without using extra space s=O(1)

--- Strings/palindrome.js
+++ Strings/palindrome.js
@@ -17,5 +17,26 @@ function isPalindrome(s) {
 
 }
 
-let res = isPalindrome("A man, a plan, a canal: Panama")
-console.log(res)
+//  second aproach for palindrome without using extra space t=O(n) s=O(1) 
+var isPalindrome = function (s) {
+    s = s.toLowerCase()
+    let j = s.length - 1;
+    let i = 0;
+
+    while(i < j){
+        if(!s[i].match(/[a-z0-9]/i)){
+            i++
+        }
+        else if(!s[j].match(/[a-z0-9]/i)){
+            j--
+        }
+        else if(s[i] === s[j]){
+           i++
+           j--
+        }
+        else{
+            return false
+        }
+    }
+    return true
+};
