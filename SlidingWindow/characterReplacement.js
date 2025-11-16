b86031345fc4ed8ac55cf0c4aba10efb95575456
meghraj thakre <meghrajthakre444@gmail.com>
2025-11-16 09:01:46 +0530

    Character Replacement

---
+++ SlidingWindow/characterReplacement.js
@@ -0,0 +1,43 @@
+
+// You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+
+// Return the length of the longest substring containing the same letter you can get after performing the above operations.
+var characterReplacement = function (s, k) {
+    let i = 0;
+    let j = 0;
+    let map = {};
+    let maxWindow = 0;
+
+    while (j < s.length) {
+
+        // Always first include s[j] in the window
+        map[s[j]] = (map[s[j]] || 0) + 1;
+
+        // If window invalid -> shrink
+        if (!isWindowValid(map, k)) {
+            map[s[i]]--;
+            i++;
+        } else {
+            maxWindow = Math.max(maxWindow, j - i + 1);
+        }
+
+        j++;
+    }
+
+    return maxWindow;
+};
+
+const isWindowValid = function (map, k) {
+    let totalCount = 0;
+    let maxCount = 0;
+
+    for (let i = 0; i < 26; i++) {
+        let char = String.fromCharCode(i + 65);
+        if (map[char]) {
+            totalCount += map[char];
+            maxCount = Math.max(maxCount, map[char]);
+        }
+    }
+
+    return (totalCount - maxCount <= k);
+};
