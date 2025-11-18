f9536ea7884c06dc978d2613c207fd7440261012
meghraj thakre <meghrajthakre444@gmail.com>
2025-11-18 16:50:24 +0530

    Sliding Window Maximum ( hard )

--- SlidingWindow/maxSlidingWindow.js
+++ SlidingWindow/maxSlidingWindow.js
@@ -1,8 +1,10 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number[]}
- */
+// 239. Sliding Window Maximum
+
+// You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+// Return the max sliding window.
+
+
 var maxSlidingWindow = function (nums, k) {
     let res = [];
     let q = [];
